c2f4c4247e10f69eb5b2575a82db16e8f1ff463d
Rakemoon <rakemoonkuzmin@gmail.com>
2025-06-13 14:23:04 +0700

    feat: style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,97 @@
+"use client";
+// src/app/page.tsx
+
+import {
+  Blink,
+  useBlink,
+  useActionsRegistryInterval,
+} from "@dialectlabs/blinks";
+
+import "@dialectlabs/blinks/index.css";
+
+import { useEvmWagmiAdapter } from "@dialectlabs/blinks/hooks/evm";
+
+import { ConnectKitButton, useModal } from "connectkit";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  // Actions registry interval
+  useActionsRegistryInterval();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  // ConnectKit modal
+  const { setOpen } = useModal();
+
+  // Wagmi adapter, used to connect to the wallet
+  const { adapter } = useEvmWagmiAdapter({
+    onConnectWalletRequest: async () => {
+      setOpen(true);
+    },
+  });
+
+  // Action we want to execute in the Blink
+  const { blink, isLoading } = useBlink({
+    url: "evm-action:http://localhost:3000/api/actions/donate-mon",
+  });
+
+  return (
+    <main className="flex gap-2 min-h-screen p-2 max-xl:flex-col-reverse">
+      <div className="w-100 self-center">
+        {isLoading || !blink ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="animate-spin mx-auto size-50"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+          </svg>
+        ) : (
+          // Blink component, used to execute the action
+          <Blink
+            blink={blink}
+            stylePreset="x-dark"
+            adapter={adapter}
+            securityLevel="all"
+          />
+        )}
+      </div>
+      <div className="flex flex-col grow">
+        <div className="flex justify-end">
+          <ConnectKitButton />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="grow flex">
+          <div className="text-5xl px-10 self-center w-max mx-auto relative">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 32 32"
+            fill="none"
+            className="absolute -bottom-30 size-20"
+          >
+            <path
+              d="M15.921 0C11.3234 0 0 11.3792 0 15.9999C0 20.6206 11.3234 32 15.921 32C20.5186 32 31.8422 20.6204 31.8422 15.9999C31.8422 11.3794 20.5188 0 15.921 0ZM13.44 25.1492C11.5012 24.6183 6.28864 15.455 6.81704 13.5066C7.34544 11.5581 16.4634 6.31979 18.4021 6.8508C20.341 7.38173 25.5535 16.5449 25.0252 18.4934C24.4968 20.4418 15.3787 25.6802 13.44 25.1492Z"
+              fill="#836EF9"
+            />
+          </svg>
+            YOUR MONEY IS{" "}
+            <span className="relative inline-block">
+              <span className="absolute top-0 left-0 -translate-y-full text-red-400">
+                SAFE
+              </span>
+              <span>GONE</span>
+              <span className="absolute top-0 bottom-0 -right-2 -left-2 my-auto h-1 bg-red-400 rounded-full rotate-5"></span>
+            </span>{" "}
+            WITH US!
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
